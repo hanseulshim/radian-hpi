65529bdbccd895bccc3b2b105568c99c4bdb3d27
joneil@boostlabs.com <joneil@boostlabs.com>
2020-10-05 15:45:28 -0500

    add hero form

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -18,14 +18,24 @@ export const Hero: React.FC<Props> = () => {
           </h2>
           <h5>
             Leveraging a wealth of housing data and machine learning, Radian
-            Home Price Index contains the most accurate jousing valuations
+            Home Price Index contains the most accurate housing valuations
             across nearly all of the U.S. housing stock. Radian valuations are
             based on "current" condition and are updated monthly. Give it a try
             below.
           </h5>
+          <div className="hero-explore">
+            <input type="text" placeholder="State, City, Zip, etc..."></input>
+            <span>
+              <button className="btn btn-light">Explore</button>
+            </span>
+          </div>
         </div>
         <div className="hero-images">
           <img src={heroImages} alt="radian hpi" />
+          <div className="featured-on">
+            <p>As featured on</p>
+            <div className="logos">Add logos here</div>
+          </div>
         </div>
       </div>
     </section>
