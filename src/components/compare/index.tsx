4c079347c1246159beb5a456f92a68207c0f7b71
joneil@boostlabs.com <joneil@boostlabs.com>
2020-10-22 13:46:19 -0500

    select competitor table

--- src/components/compare/index.tsx
+++ src/components/compare/index.tsx
@@ -1,12 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ContentContext } from 'components/App'
 
 interface Props {}
 
+interface Competitor {
+  name: string | null
+  indexType: string | null
+  observations: string | null
+  dataFrequency: string | null
+  monthlyObservations: string | null
+  propertyTransactionExclusions: Array<string> | null
+  indexDeliveryDelay: string | null
+  propertyCondition: string | null
+  imposedModelAdjustments: string | null
+  propertyType: Array<string> | null
+  statesInNationalIndex: string | null
+}
+
 export const Compare: React.FC<Props> = () => {
   const { compare } = useContext(ContentContext)
+  const [competitor, setCompetitor] = useState<Competitor | null>(null)
 
   if (compare) {
+    const { competitors, radianHpi } = compare
+
+    const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const competitorIndex = parseInt(e.target.value)
+      const selectedCompetitor = competitors?.find(
+        (comp, index) => index === competitorIndex
+      )
+      if (selectedCompetitor) {
+        setCompetitor(selectedCompetitor)
+      }
+    }
+
     return (
       <section className="container-fluid compare-container">
         <div className="row">
@@ -25,14 +52,14 @@ export const Compare: React.FC<Props> = () => {
             <div className="hpi-tool-select">
               <h5>Are you currently using an index tool?</h5>
               <p>If yes, choose a tool</p>
-              <select className="custom-select">
-                <option defaultValue="Choose an app...">
+              <select className="custom-select" onChange={onChange}>
+                <option disabled selected value={undefined}>
                   Choose an app...
                 </option>
-                {compare.competitors?.map((competitor, idx) => {
+                {competitors?.map((competitor, idx) => {
                   return (
                     <option value={idx} key={'competitor' + idx}>
-                      {competitor}
+                      {competitor.name}
                     </option>
                   )
                 })}
@@ -44,34 +71,92 @@ export const Compare: React.FC<Props> = () => {
               <thead>
                 <tr>
                   <th></th>
-                  <th>Radian HPI</th>
+                  <th>{radianHpi?.name}</th>
+                  {competitor && <th>{competitor.name}</th>}
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <th>Index Type</th>
-                  <td>Cell</td>
-                  <td>Cell</td>
+                  <td>{radianHpi?.indexType}</td>
+                  {competitor && <td>{competitor.indexType}</td>}
                 </tr>
                 <tr>
                   <th>Observations</th>
-                  <td>Cell</td>
-                  <td>Cell</td>
+                  <td>{radianHpi?.observations}</td>
+                  {competitor && <td>{competitor.observations}</td>}
                 </tr>
                 <tr>
                   <th>Data Frequency</th>
-                  <td>Cell</td>
-                  <td>Cell</td>
+                  <td>{radianHpi?.dataFrequency}</td>
+                  {competitor && <td>{competitor.dataFrequency}</td>}
                 </tr>
                 <tr>
                   <th>Monthly Observations</th>
-                  <td>Cell</td>
-                  <td>Cell</td>
+                  <td>{radianHpi?.monthlyObservations}</td>
+                  {competitor && <td>{competitor.monthlyObservations}</td>}
                 </tr>
                 <tr>
                   <th>Property and Transaction Exclusions</th>
-                  <td>Cell</td>
-                  <td>Cell</td>
+                  <td>
+                    {radianHpi?.propertyTransactionExclusions
+                      ? radianHpi?.propertyTransactionExclusions.map(
+                          (excl, idx) => {
+                            return <li key={'excl' + idx}>{excl}</li>
+                          }
+                        )
+                      : 'None'}
+                  </td>
+                  {competitor && (
+                    <td>
+                      {competitor.propertyTransactionExclusions
+                        ? competitor?.propertyTransactionExclusions.map(
+                            (excl, idx) => {
+                              return <li key={'comp-excl' + idx}>{excl}</li>
+                            }
+                          )
+                        : 'None'}
+                    </td>
+                  )}
+                </tr>
+                <tr>
+                  <th>Index Delivery Delay</th>
+                  <td>{radianHpi?.indexDeliveryDelay}</td>
+                  {competitor && <td>{competitor.indexDeliveryDelay}</td>}
+                </tr>
+                <tr>
+                  <th>Property Condition</th>
+                  <td>{radianHpi?.propertyCondition}</td>
+                  {competitor && <td>{competitor.propertyCondition}</td>}
+                </tr>
+                <tr>
+                  <th>Imposed Model Adjustments</th>
+                  <td>{radianHpi?.imposedModelAdjustments}</td>
+                  {competitor && <td>{competitor.imposedModelAdjustments}</td>}
+                </tr>
+                <tr>
+                  <th>Property Type</th>
+                  <td>
+                    {radianHpi?.propertyType
+                      ? radianHpi?.propertyType.map((type, idx) => {
+                          return <li key={'type' + idx}>{type}</li>
+                        })
+                      : 'None'}
+                  </td>
+                  {competitor && (
+                    <td>
+                      {competitor.propertyType
+                        ? competitor?.propertyType.map((excl, idx) => {
+                            return <li key={'comp-type' + idx}>{excl}</li>
+                          })
+                        : 'None'}
+                    </td>
+                  )}
+                </tr>
+                <tr>
+                  <th>States in National Index</th>
+                  <td>{radianHpi?.statesInNationalIndex}</td>
+                  {competitor && <td>{competitor.statesInNationalIndex}</td>}
                 </tr>
               </tbody>
             </table>
--- src/initialState.tsx
+++ src/initialState.tsx
@@ -1,12 +1,12 @@
 export const initialState = {
   hero: null,
-  learningCenter:null,
-  media:null,
-  compare:null,
+  learningCenter: null,
+  media: null,
+  compare: null
 }
 
 export interface DataInterface {
-	hero: {
+  hero: {
     title: string | null
     subtitle: string | null
     description: string | null
@@ -28,12 +28,37 @@ export interface DataInterface {
     downloadTitle: string | null
     downloadLink: string | null
   } | null
-  compare:{
+  compare: {
     titleIcon: string | null
     title: string | null
     subtitle: string | null
     instructions: string | null
-    competitors: Array<string> | null
+    competitors: Array<{
+      name: string | null
+      indexType: string | null
+      observations: string | null
+      dataFrequency: string | null
+      monthlyObservations: string | null
+      propertyTransactionExclusions: Array<string> | null
+      indexDeliveryDelay: string | null
+      propertyCondition: string | null
+      imposedModelAdjustments: string | null
+      propertyType: Array<string> | null
+      statesInNationalIndex: string | null
+    }> | null
+    radianHpi: {
+      name: string | null
+      indexType: string | null
+      observations: string | null
+      dataFrequency: string | null
+      monthlyObservations: string | null
+      propertyTransactionExclusions: Array<string> | null
+      indexDeliveryDelay: string | null
+      propertyCondition: string | null
+      imposedModelAdjustments: string | null
+      propertyType: Array<string> | null
+      statesInNationalIndex: string | null
+    } | null
     backgroundImage: string | null
   } | null
 }
