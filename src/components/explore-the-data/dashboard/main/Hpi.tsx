66dcd0dcdecb8b179a38d56812200a5019ef9fd1
joneil@boostlabs.com <joneil@boostlabs.com>
2020-11-06 09:54:12 -0600

    add animations

--- src/components/explore-the-data/dashboard/main/Hpi.tsx
+++ src/components/explore-the-data/dashboard/main/Hpi.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as am4core from '@amcharts/amcharts4/core'
 import * as am4charts from '@amcharts/amcharts4/charts'
+import am4themes_animated from '@amcharts/amcharts4/themes/animated'
 
 import { dashboardHpi } from 'api'
+am4core.useTheme(am4themes_animated)
 am4core.options.commercialLicense = true
 
 interface Geo {
@@ -19,62 +21,67 @@ interface Props {
 
 export const Hpi: React.FC<Props> = ({ startDate, endDate, range, geos }) => {
   const [dataOption, setDataOption] = useState('hpi')
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await dashboardHpi({
+        startDate: !range ? startDate : null,
+        endDate: !range ? endDate : null,
+        range: range || null,
+        locations: geos
+      })
+      setData(data)
+    }
+    getData()
+  }, [endDate, geos, range, startDate, dataOption])
 
   useEffect(() => {
     let chart = am4core.create('hpi-chart', am4charts.XYChart)
-    const buildChart = async () => {
-      if (geos.length) {
-        const data = await dashboardHpi({
-          startDate: !range ? startDate : null,
-          endDate: !range ? endDate : null,
-          range: range || null,
-          locations: geos
-        })
-        chart.data = data
-        chart.paddingLeft = 0
 
-        let dateAxis = chart.xAxes.push(new am4charts.DateAxis())
-        dateAxis.startLocation = 0.5
-        dateAxis.endLocation = 0
-        dateAxis.renderer.minGridDistance = 50
-        dateAxis.renderer.labels.template.location = 0.0001
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    chart.data = data
+    chart.paddingLeft = 0
 
-        const createSeries = (
-          name: string,
-          data: Array<{ date: string; hpi: number; median: number }>,
-          index: number
-        ) => {
-          let series = chart.series.push(new am4charts.LineSeries())
-          series.dataFields.valueY = dataOption
-          series.dataFields.dateX = 'date'
-          series.name = name
-          series.data = data
-          const colors = [
-            '#002b49',
-            '#00bab3',
-            '#ffc882',
-            '#9bb9b4',
-            '#820933',
-            '#fa7268z'
-          ]
-          series.stroke = am4core.color(colors[index])
-          series.strokeWidth = 2.5
-          series.tensionX = 0.9
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis())
+    dateAxis.startLocation = 0.5
+    dateAxis.endLocation = 0
+    dateAxis.renderer.minGridDistance = 50
+    dateAxis.renderer.labels.template.location = 0.0001
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
 
-          return series
-        }
+    const createSeries = (
+      name: string,
+      data: Array<{ date: string; hpi: number; median: number }>,
+      index: number
+    ) => {
+      let series = chart.series.push(new am4charts.LineSeries())
+      series.dataFields.valueY = dataOption
+      series.dataFields.dateX = 'date'
+      series.name = name
+      series.data = data
+      const colors = [
+        '#002b49',
+        '#00bab3',
+        '#ffc882',
+        '#9bb9b4',
+        '#820933',
+        '#fa7268z'
+      ]
+      series.stroke = am4core.color(colors[index])
+      series.strokeWidth = 2.5
+      series.tensionX = 0.9
 
-        chart.data.forEach((cohort, index) =>
-          createSeries(cohort.label, cohort.data, index)
-        )
-      }
+      return series
     }
-    buildChart()
+
+    chart.data.forEach((cohort, index) =>
+      createSeries(cohort.label, cohort.data, index)
+    )
+
     return () => {
       chart.dispose()
     }
-  }, [endDate, geos, range, startDate, dataOption])
+  }, [data, dataOption])
   return (
     <div className="hpi-chart-container">
       <div className="title-and-controls">
